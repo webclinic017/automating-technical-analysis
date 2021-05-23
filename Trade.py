a4ebd730566d40c7b9e4fbbb98fc007e4f95a6c1
Andrew Kurgat <47039863+akurgat@users.noreply.github.com>
2021-05-23 11:31:11 +0300

    Initial commit

---
+++ Trade.py
@@ -0,0 +1,128 @@
+from app.data_sourcing import Data_Sourcing, data_update
+from app.graph import Visualization
+from tensorflow.keras.models import load_model
+import streamlit as st 
+
+data_update()
+action_model = load_model("models/action_prediction_model.h5")
+price_model = load_model("models/price_prediction_model.h5")
+app_data = Data_Sourcing()
+
+def main():
+    st.set_page_config(layout = "wide")
+    st.sidebar.subheader('Indication:')
+    indication = st.sidebar.selectbox('', ('Predicted', 'Analysed'))
+    
+    #st.sidebar.subheader('Exchange:')
+    exchange = 'Binance'
+    app_data.exchange_data(exchange)
+
+    if exchange == 'Yahoo! Finance':
+        assets = app_data.stocks
+        
+        st.sidebar.subheader('Stock:')
+        asset = st.sidebar.selectbox('', assets)
+        currency = app_data.df_stocks[(app_data.df_stocks['Company'] == asset)]['Currency'].unique()[0]
+        market = app_data.df_stocks[(app_data.df_stocks['Company'] == asset)]['Currency_Name'].unique()[0]
+            
+        st.sidebar.subheader('Interval:')
+        interval = st.sidebar.selectbox('', ('1 Hour', '1 Day', '1 Week'))        
+        label = 'Stock'
+    else:
+        markets = app_data.markets
+        
+        st.sidebar.subheader('Market:')
+        market = st.sidebar.selectbox('', markets)
+        app_data.market_data(market)
+        assets = app_data.assets
+        currency = app_data.currency
+        
+        st.sidebar.subheader('Crypto:')
+        asset = st.sidebar.selectbox('', assets)
+
+        st.sidebar.subheader('Interval:')
+        if exchange == 'Binance':
+            interval = st.sidebar.selectbox('', ('5 Minute', '15 Minute', '30 Minute', '1 Hour', '1 Day'))
+        else:
+            interval = st.sidebar.selectbox('', ('5 Minute', '30 Minute', '1 Hour', '1 Day'))
+        label = 'Cryptocurrency'
+        
+    st.sidebar.subheader('Trading Risk:')
+    risk = st.sidebar.selectbox('', ('Low', 'Medium', 'High'))
+
+    st.title(f'Automated Technical Analysis for {label} Trading.')
+    st.subheader(f'{label} Data Sourced from {exchange} in {interval} Interval.')
+    st.info(f'Predicting...')
+    
+    future_price = 30   
+    analysis = Visualization(exchange, interval, asset, indication, action_model, price_model, market)
+    requested_date = analysis.df.index[-1]
+    current_price = float(analysis.df['Adj Close'][-1])
+    requested_prediction_price = float(analysis.requested_prediction_price)
+    requested_prediction_action = analysis.requested_prediction_action
+
+    risks = {'Low': [analysis.df['S1'].values[-1], analysis.df['R1'].values[-1]], 
+            'Medium': [analysis.df['S2'].values[-1], analysis.df['R2'].values[-1]],   
+            'High': [analysis.df['S3'].values[-1], analysis.df['R3'].values[-1]],}
+    buy_price = float(risks[risk][0])
+    sell_price = float(risks[risk][1])
+
+    if label == 'Stock':
+        current_price = f'{float(current_price):,.2f}'
+        requested_prediction_price = f'{float(requested_prediction_price):,.2f}'
+        buy_price = f'{float(buy_price):,.2f}'
+        sell_price = f'{float(sell_price):,.2f}'
+    else:
+        current_price = f'{float(current_price):,.8f}'
+        requested_prediction_price = f'{float(requested_prediction_price):,.8f}'
+        buy_price = f'{float(buy_price):,.8f}'
+        sell_price = f'{float(sell_price):,.8f}'
+
+    if analysis.requested_prediction_action == 'Hold':
+        present_statement_prefix = 'off from taking any action with'
+        present_statement_suffix = ' at this time'
+    else:
+        present_statement_prefix = ''
+        present_statement_suffix = ''
+                
+    accuracy_threshold = {analysis.score_action: 70., analysis.score_price: 50.}
+    confidence = dict()
+    for score, threshold in accuracy_threshold.items():
+        if float(score) >= threshold:
+            confidence[score] = f'*({score}% confident.)*'
+        else:
+            confidence[score] = ''
+
+    st.markdown(f'**Prediction Date & Time (UTC):** {str(requested_date)}.')
+    st.markdown(f'**Current Price:** {currency} {current_price}.')
+    st.markdown(f'**Current Trading Action Recommendation:** You should **{requested_prediction_action.lower()}** {present_statement_prefix} this {label.lower()[:6]}{present_statement_suffix}. {str(confidence[analysis.score_action])}')
+    st.markdown(f'**Future Price Estimation:** The {label.lower()[:6]} price for  **{asset}** is estimated to be **{currency} {requested_prediction_price}** in the next **{int(interval.split()[0]) * future_price} {str(interval.split()[1]).lower()}s**. {str(confidence[analysis.score_price])}')
+    if requested_prediction_action == 'Hold':
+        st.markdown(f'**Trading Recommendation:** You should consider buying more **{asset}** {label.lower()[:6]} at **{currency} {buy_price}** and sell it at **{currency} {sell_price}**.')
+
+    prediction_fig = analysis.prediction_graph()
+    if indication == 'Predicted':
+        testing_prefix = 'Predicted'
+    else:
+        testing_prefix = 'Analysed'
+
+    st.success(f'Historical {label[:6]} Price Action...({testing_prefix}.)')
+    st.plotly_chart(prediction_fig, use_container_width = True)
+
+    technical_analysis_fig = analysis.technical_analysis_graph()
+    st.success (f'Technical Analysis results from the {label[:6]} Data...')
+    st.plotly_chart(technical_analysis_fig, use_container_width = True)
+
+    st.sidebar.info('Other Options:')
+    if st.sidebar.checkbox('The Sourced Data'):
+        st.success ('Sourcing...')
+        st.markdown(f'Sourced {label} Data.')
+        st.write(analysis.df[['High', 'Low', 'Open', 'Volume', 'Adj Close']].tail(10)) 
+
+        
+if __name__ == '__main__':
+    import warnings
+    import gc
+    warnings.filterwarnings("ignore")
+    main()
+    gc.collect()
---
+++ app/model.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pandas as pd
+from app.scaling import Preprocessing
+from sklearn.preprocessing import MultiLabelBinarizer, StandardScaler
+from sklearn.metrics import r2_score
+import datetime as dt
+
+pd.set_option("display.precision", 8)
+
+class Prediction(Preprocessing):
+    
+    def __init__(self, exchange, interval, asset, action_model, price_model, market = None):
+        super().__init__(exchange, interval, asset, market)
+
+        self.start_date = self.df.index[-3]
+        self.action_model = action_model
+        self.price_model = price_model
+
+        features = ['High', 'Low', 'Open', 'Volume', 'Adj Close', 'P', 'R1', 'R2', 'R3', 'S1', 'S2', 'S3', 
+                    'OBV', 'MACD', 'MACDS', 'MACDH', 'SMA', 'LMA', 'RSI', 'SR_K', 'SR_D', 'HL_PCT', 'PCT_CHG']
+        self.df_action = self.df.copy()[features + ['Distinct_Action']]
+        self.df_price = self.df.copy()[features + ['Future_Adj_Close']]
+
+        self.scaler = StandardScaler()
+        self.df_price['Future_Adj_Close_Scaled'] = self.scaler.fit_transform(self.df_price[['Future_Adj_Close']].values).reshape(-1)
+        self.action_features, self.action_labels = super(Prediction, self).scaling(self.df_action)
+        self.price_features, self.price_labels = super(Prediction, self).scaling(self.df_price[features + ['Future_Adj_Close_Scaled']])
+        self.mlb = MultiLabelBinarizer(classes = ['Buy', 'Hold', 'Sell'])
+        self.action_labels = self.mlb.fit_transform(self.action_labels)
+        
+    def get_prediction(self):         
+        self.model_prediction_action = self.action_model.predict(self.action_features)
+        self.model_prediction_price = self.price_model.predict(self.price_features)
+        
+        self.model_prediction_action = np.array(self.mlb.inverse_transform(self.model_prediction_action.round()))
+        self.model_prediction_price = self.scaler.inverse_transform(self.model_prediction_price).flatten()
+        self.requested_prediction_action = str(self.model_prediction_action[-1][-1])
+        self.requested_prediction_price = round(float(self.model_prediction_price[-1]), 8)
+
+        price_prediction_length = self.model_prediction_price.shape[0]
+        self.df_price = self.df_price.iloc[-price_prediction_length:]
+
+        self.score_action = self.action_model.evaluate(self.action_features, self.action_labels, verbose = 0) 
+        self.score_price = r2_score(self.df_price['Future_Adj_Close'].values[:-30], self.model_prediction_price[:-30])
+        self.score_action, self.score_price = round((self.score_action[1] * 100), 2), round((self.score_price * 100), 2)
+        
+    def prediction_postprocessing(self, indication):
+        self.indication = indication
+        indicators = {'Analysed':'Distinct_Action', 'Predicted':'Action_Predictions'}
+
+        action_prediction_length = self.model_prediction_action.shape[0]
+        self.df_visulization = self.df.iloc[-action_prediction_length:]
+        self.df_visulization['Action_Predictions'] = self.model_prediction_action
+        self.df_visulization = self.df_visulization[['Adj Close', 'Volume', 'Distinct_Action', 
+                                                     'Action_Predictions', 'Future_Adj_Close']]
+        #self.df_visulization = self.df_visulization.iloc[-366:]
+        self.df_visulization['Price_Buy'] = self.df_visulization[self.df_visulization[indicators[self.indication]] == 'Buy']['Adj Close']
+        self.df_visulization['Price_Sell'] = self.df_visulization[self.df_visulization[indicators[self.indication]] == 'Sell']['Adj Close']
+
+        interval_value = int(self.selected_interval.split()[0])
+        interval_time = str(self.selected_interval.split()[1]).lower()
+
+        future_date = []
+        for value in range(1, 34):
+            if interval_time == 'minute':
+                future_date.append(self.start_date + dt.timedelta(minutes = value * interval_value))
+            elif interval_time == 'hour':
+                future_date.append(self.start_date + dt.timedelta(hours = value * interval_value))
+            elif interval_time == 'day':
+                future_date.append(self.start_date + dt.timedelta(days = value * interval_value))
+            elif interval_time == 'week':
+                future_date.append(self.start_date + dt.timedelta(weeks = value * interval_value))
+
+        self.df_future_price = pd.DataFrame(future_date, columns = ['Date'])
+        self.df_future_price['Future Price'] = self.model_prediction_price[-33:].reshape(-1)
+        self.df_future_price.set_index('Date', inplace = True)
